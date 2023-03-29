4e6cd6b99898938ff693f5ce66d35273bb9fd5f3
toprakkis <93645179+toprakkis@users.noreply.github.com>
2023-03-29 23:03:46 +0200

    First Chapter of the Course

--- Pandas/Pandas_1.py
+++ Pandas/Pandas_1.py
@@ -1,2 +1,10 @@
 # First Pandas course file
 import pandas as pd
+import numpy as np
+# With below line you can see pandas version
+version = (pd.__version__)
+my_data = [10, 20, 30]
+my_index = [1, 2, 3]
+
+my_serie = pd.Series(data=my_data, index=my_index)
+print(my_serie)
