0bdd93a537c7dea1d37dc1ad31ea528603e3e137
toprakkis <93645179+toprakkis@users.noreply.github.com>
2023-03-29 23:09:53 +0200

    Rapid pushes to increase my familiarity

--- Pandas/Pandas_1.py
+++ Pandas/Pandas_1.py
@@ -6,5 +6,15 @@ version = (pd.__version__)
 my_data = [10, 20, 30]
 my_index = [1, 2, 3]
 
+# Supplying data and index to Pandas Series
 my_serie = pd.Series(data=my_data, index=my_index)
+
+sample_dict = {"a": 10, "b": 20}
+
+# Pandas Series takes keys as index and values as data automatically
+
+my_serie = pd.Series(data=sample_dict)
 print(my_serie)
+
+# .keys() gives indexes of the Pandas Serie
+print(my_serie.keys()
